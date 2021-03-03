68be5ba6db589db17f0d7cb9dd4a1f735fe46a17
Carrilhos <gcarrilhos@gmail.com>
2021-03-03 15:56:08 -0300

    var let const

---
+++ desafio 8/types.js
@@ -0,0 +1,19 @@
+let um = "número oito";
+console.log(typeof um);
+
+let dois = 9;
+console.log(typeof dois);
+
+let tres = true;
+console.log(typeof tres);
+
+let quatro = null;
+console.log(typeof quatro);
+
+let cinco = undefined;
+console.log(typeof cinco);
+
+//ou
+
+let seis
+console.log(typeof seis);
